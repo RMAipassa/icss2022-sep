6ca196ae5007e6916064cc1471ba7a922f27645b
Ruben Aipassa <rm.aipassa@student.han.nl>
2024-10-31 21:10:49 +0100

    Start on evaluator

--- startcode/src/main/java/nl/han/ica/icss/transforms/Evaluator.java
+++ startcode/src/main/java/nl/han/ica/icss/transforms/Evaluator.java
@@ -1,5 +1,6 @@
 package nl.han.ica.icss.transforms;
 
+import nl.han.ica.datastructures.HANLinkedList;
 import nl.han.ica.datastructures.IHANLinkedList;
 import nl.han.ica.icss.ast.*;
 import nl.han.ica.icss.ast.literals.PercentageLiteral;
@@ -17,14 +18,33 @@ public class Evaluator implements Transform {
     private IHANLinkedList<HashMap<String, Literal>> variableValues;
 
     public Evaluator() {
-        //variableValues = new HANLinkedList<>();
+        variableValues = new HANLinkedList<>();
     }
 
     @Override
     public void apply(AST ast) {
-        //variableValues = new HANLinkedList<>();
-
+        variableValues = new HANLinkedList<>();
+        applyStylesheet((Stylesheet) ast.root);
+    }
+    // Onderdelen
+    private void applyStylesheet(Stylesheet node) {
+        applyStylerule((Stylerule) node.getChildren().get(0));
+    }
+    private void applyStylerule(Stylerule node) {
+        for (ASTNode child : node.getChildren()) {
+            if (child instanceof Declaration) {
+                applyDeclaration((Declaration) child);
+            }
+        }
+    }
+    private void applyDeclaration(Declaration node) {
+        node.expression = evalExpression(node.expression);}
+    // ??? TODO: Wat moet ik evalueren aan deze onderdelen?
+    private Expression evalExpression(Expression expression) {
+        return null;
     }
 
-    
+}
+
+
 }
